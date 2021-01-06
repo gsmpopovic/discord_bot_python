7c792a6b41b9f23c1f473d40117c830c8d509ef8
George Popovic <georgesmpopovic@gmail.com>
2021-01-05 19:06:25 -0500

    Using bot commands now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import mpdules
 import os
 import discord
+from discord.ext import commands
 #from dotenv import load_dotenv
 import requests
 import json
@@ -26,12 +27,12 @@ GUILD = os.getenv('GUILD_NAME')
 # A Client handles events, tracks state,
 # and generally interacts with Discord APIs.
 client = discord.Client()
-
+bot = commands.Bot(command_prefix='!')
 ########################################################################
 
 #Function declarations
-
-def get_quote():
+@bot.command(name="quote", help="Will display a random quote.")
+async def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_response = json.loads(response.text) 
     quote = ' Quote: ' + json_response[0]['q'] + ' - ' + json_response[0]['a']
@@ -40,20 +41,22 @@ def get_quote():
 # greet_user greets user by name. 
 # Takes as an argument 
 # message.author.name (name of message sender)
-
-def greet_user(name):
+@bot.command(name="greet", help="Greets user by name.")
+async def greet_user(name):
     greeting = "Oh, hi there " + name + "!"
     return greeting
 
-def update_commands(command):
+@bot.command(name="update_commands", help="Create new command based on user inputting a string--name of new command.")
+async def update_commands(command):
     if "commands" in db.keys():
         commands = db["commands"]
         commands.append(command)
         db["commands"] = commands
     else:
         db["commands"] = ["commands"]
-    
-def delete_commands(index):
+
+@bot.command(name="delete_commands", help="Will delete command based on user input--user input will be index of command to be deleted")
+async def delete_commands(index):
   commands = db["commands"]
   if len(commands) > index:
     del commands[index]
@@ -95,21 +98,21 @@ async def on_message(message):
 
         if msg.startswith("/newcmd"):
             new_command = msg.split("/newcmd ", 1)[1]
-            update_commands(new_command)
+            await update_commands(new_command)
             await message.channel.send(f"New command '{new_command}' was created!")
 
         if msg.startswith("/delcmd"):
             commands = []
             if "commands" in db.keys():
               index = int(msg.split("/delcmd", 1)[1])
-              delete_commands(index)
+              await delete_commands(index)
               commands = db["commands"]
               await message.channel.send("Okay. Here are all the commands I have left over: ")
               await message.channel.send(commands)
 
         if msg == "/greet":
             name = message.author.name
-            greeting = greet_user(name)
+            greeting = await greet_user(name)
             await message.channel.send(greeting)
         
         elif msg == "/list":
@@ -136,7 +139,8 @@ async def on_message(message):
                 await msg.delete()
 
         elif msg == "/inspire":
-            quote = get_quote()
+            quote = await get_quote()
+            return quote 
             
 ############################################################################
 client.run('Nzg5Mjk4NDczNzM1ODE1MTg3.X9wBfA.RtMMwbcw9goAx6IqzTNqIP5Xh-Q')
